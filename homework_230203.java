2e01333163194e00b584ccd2783719a83b02b54d
DESKTOP-JQDL6G8\theCHAIN <khgm234@gmail.com>
2023-02-03 16:34:43 +0900

    sample

---
+++ homework_230203.java
@@ -0,0 +1,19 @@
+public class homework_230203 {
+    public class Bus{
+        int maxPassenger = 0;
+        int currentPassenger = 0;
+        int charge = 0;
+        int busNumber = 0;
+        int oilTank = 0;
+        int Speed = 0;
+        boolean State = true;
+    }
+
+    class Main {
+        public void main(String args[]) {
+            Bus transportation = new Bus;
+
+            System.out.println(transportation);
+        }
+    }
+}
